f1338dbb8a5a57f88c9c89ef60435204e5336f7b
sunzw <779457649@qq.com>
2023-10-16 17:17:58 +0800

    Update bpacking_signed.hpp

--- bpacking_signed.hpp
+++ bpacking_signed.hpp
@@ -1,11 +1,3 @@
-/**
- *
- * This code is released under the
- * Apache License Version 2.0 http://www.apache.org/licenses/.
- * (c) Daniel Lemire 2013
- */
-
-// #include "bpacking.h"
 #include <cstdint>
 
 
