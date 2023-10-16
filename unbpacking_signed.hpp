83dca637454dd52b4789e0ec1d0e186b47479a76
sunzw <779457649@qq.com>
2023-10-16 17:19:14 +0800

    Update unbpacking_signed.hpp

--- unbpacking_signed.hpp
+++ unbpacking_signed.hpp
@@ -1,11 +1,4 @@
-/**
- *
- * This code is released under the
- * Apache License Version 2.0 http://www.apache.org/licenses/.
- * (c) Daniel Lemire 2013
- */
 
-// #include "bpacking.h"
 
 
 namespace compression
